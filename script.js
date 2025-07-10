218e4de8d62efe30f5b2fbde6f60efdeb3eaa3dc
Nick <mail-trash@online.de>
2025-07-10 17:09:46 +0200

    Add src to hashInput

--- script.js
+++ script.js
@@ -29,10 +29,10 @@ function confirmInput() {
     }, 3000);
 }
 
-async function hashInput(text) {
+async function hashInput(text) { // https://developer.mozilla.org/en-US/docs/Web/API/SubtleCrypto/digest
     const encoder = new TextEncoder();
     const data = encoder.encode(text);
-    const hashBuffer = await crypto.subtle.digest("SHA-256", data);
+    const hashBuffer = await window.crypto.subtle.digest("SHA-256", data);
     const hashArray = Array.from(new Uint8Array(hashBuffer));
     return hashArray.map((b) => b.toString(16).padStart(2, "0")).join("");
 }
